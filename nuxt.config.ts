f49de1f85f0eb933e2693ecdc01958cded32b61c
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-07-12 19:42:37 +0200

    feat: Add initial components and templates for dashboard, login, and confirmation pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [// https://github.com/nuxt-modules/supabase
+  '@nuxtjs/supabase', // UI lib (will be soon open sourced)
+  '@nuxthq/ui', // https://github.com/nuxt-modules/color-mode
+  '@nuxtjs/color-mode'],
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    },
+  },
+  ui: {
+    colors: {
+      primary: 'green',
+    },
+    icons: ['mdi', 'heroicons', 'heroicons-outline'],
+  },
+  supabase: {
+    redirectOptions: {
+      login: '/',
+      callback: '/confirm',
+    },
+  },
 })
