14f80d901e5274953271dea09fbdd97240457174
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-07-26 22:44:23 +0200

    feat: ✨ conditional cron tasks

--- server/plugins/scheduler.ts
+++ server/plugins/scheduler.ts
@@ -4,14 +4,15 @@ export default defineNitroPlugin(() => {
   if (process.env.APP_CRON !== 'local') {
     console.log('[server/plugins/scheduler.ts] Skipping scheduler, don\'t run in build context');
     return;
+  } else {
+    cron.schedule('*/20 * * * * *', async () => {
+      console.log('🟢 Summarizing emails every 20 seconds')
+      await $fetch('/api/summaryEmail')
+    })
+    cron.schedule('*/60 * * * * *', async () => {
+      console.log('⚪ Sending emails every 60 seconds ')
+      const status = await $fetch('/api/sendEmail')
+      console.log('Email send status:', status)
+    })
   }
-  cron.schedule('*/20 * * * * *', async () => {
-    console.log('🟢 Summarizing emails every 20 seconds')
-    await $fetch('/api/summaryEmail')
-  })
-  cron.schedule('*/60 * * * * *', async () => {
-    console.log('⚪ Sending emails every 60 seconds ')
-    const status = await $fetch('/api/sendEmail')
-    console.log('Email send status:', status)
-  })
 })
