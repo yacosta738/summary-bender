bd1fdfd363975f27d4ad95e571d0f1a4048f905f
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-07-26 18:25:38 +0200

    feat: ✨ change the scheduler plugin to solve issue in the build

--- server/plugins/scheduler.ts
+++ server/plugins/scheduler.ts
@@ -1,17 +1,17 @@
 import cron from 'node-cron'
 
 export default defineNitroPlugin(() => {
-  // cron.schedule('*/10 * * * * *', async () => {
-  //   console.log('🔵 Syncing emails every 10 seconds')
-  //   await $fetch('/api/syncEmails')
-  // })
+  if (process.env.APP_ENV === 'build') {
+    console.log('[server/plugins/scheduler.ts] Skipping scheduler, don\'t run in build context');
+    return;
+  }
   cron.schedule('*/20 * * * * *', async () => {
     console.log('🟢 Summarizing emails every 20 seconds')
     await $fetch('/api/summaryEmail')
   })
   cron.schedule('*/60 * * * * *', async () => {
     console.log('⚪ Sending emails every 60 seconds ')
-    const status =  await $fetch('/api/sendEmail')
+    const status = await $fetch('/api/sendEmail')
     console.log('Email send status:', status)
   })
 })
