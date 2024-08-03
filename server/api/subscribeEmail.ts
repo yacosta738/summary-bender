928f960e6efd011f30d51949839ac573770ad4be
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-08-03 11:16:05 +0200

    feat: app mounted

--- server/api/subscribeEmail.ts
+++ server/api/subscribeEmail.ts
@@ -2,6 +2,7 @@ import EmailService from "~/services/email/emailService";
 import {processEmail} from "~/services/email/processEmailService";
 
 export default defineEventHandler(async (event) => {
+  console.log('🔵 Subscribing to email service...')
   const emailService = new EmailService(processEmail, event);
   emailService.connect();
 })
